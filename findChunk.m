fa51f7cc132e1fa25a8d6dff6fb582b7818c61c1
lnumerow <logan-numerow@hotmail.com>
2017-09-21 16:19:34 -0700

    commit

--- findChunk.m
+++ findChunk.m
@@ -4,9 +4,11 @@ function chunkIdx = findChunk(chunk,song,Fs)
     Nc = numel(song)*Fc/Fs;
     Lc = Fs/Fc;
     chunks = reshape(song,[Lc, Nc]);
+    
+    betterSong = [];
     for jj = 1:Nc
         chunk = chunks(:,jj);
-        sound(chunk,Fs);
+        %sound(chunk,Fs);
         F = (0:Lc-1)*Fc;
         fftc = abs(fft(chunk));
         F = F(1:ceil(end/2));
@@ -16,9 +18,19 @@ function chunkIdx = findChunk(chunk,song,Fs)
         F = F(idx);
         [~,idx] = sort(fftc,'descend');
         idx = idx(1:20);
-        plot(F,fftc,'-k',F(idx),fftc(idx),'or');
-        drawnow;
-        pause(0.1);
+        %plot(F,fftc,'-k',F(idx),fftc(idx),'or');
+        %drawnow;
+        
+        x = (1:numel(chunk))./Fs;
+        y = 0*x;
+        for ii=idx'
+            y = y + sin(2*pi*F(ii)*x)*fftc(ii);
+        end
+        betterSong = [betterSong y];
+        
+        %pause(0.1);
     end
+    
+    sound(betterSong,Fs);
 end
 
